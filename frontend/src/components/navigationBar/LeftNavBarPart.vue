06b6f4d075610e843928822e69e315374ff07994
Roman Kolesnikov <roman.kolesnikov@hs-augsburg.de>
2022-04-07 11:46:44 +0200

    [#4] Create left part of navigationbar

---
+++ frontend/src/components/navigationBar/LeftNavBarPart.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps<{
+  title: string;
+  logoUrl: string;
+}>();
+</script>
+
+<template>
+  <div class="flex flex-row space-x-4">
+    <img :src="logoUrl" class="w-12 h-12 rounded-full" />
+
+    <div
+      class="text-3xl text-gray-900 font-bold font-sans self-center hidden md:block"
+    >
+      {{ title }}
+    </div>
+  </div>
+</template>
---
+++ frontend/src/components/navigationBar/NavigationBar.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import LeftNavBarPart from "./LeftNavBarPart.vue";
+
+defineProps<{
+  title: string;
+  logoUrl: string;
+}>();
+</script>
+
+<template>
+  <div class="md:container md:mx-auto p-3">
+    <LeftNavBarPart :title="title" :logo-url="logoUrl" />
+  </div>
+</template>
