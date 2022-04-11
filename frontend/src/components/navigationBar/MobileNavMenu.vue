827bbdab068a74d8f333083344fea8165b6847c2
Roman Kolesnikov <roman.kolesnikov@hs-augsburg.de>
2022-04-11 16:01:52 +0200

    [#5] Add dark mode for navigation

--- frontend/src/components/navigationBar/MobileNavMenu.vue
+++ frontend/src/components/navigationBar/MobileNavMenu.vue
@@ -4,17 +4,21 @@ const menuItems = ["Categories", "Archive"];
 
 <template>
   <div class="md:hidden">
-    <div class="flex flex-col py-4 space-y-4 items-center bg-gray-50">
+    <div class="mobileMenu">
       <a v-for="item in menuItems" :key="item" class="menuItem" href="">
         {{ item }}
       </a>
     </div>
-    <div class="h-px bg-gray-200" />
+    <div class="h-px bg-gray-200 dark:bg-gray-700" />
   </div>
 </template>
 
 <style scoped>
+.mobileMenu {
+  @apply flex flex-col py-4 space-y-4 items-center bg-gray-50 dark:bg-gray-800;
+}
+
 .menuItem {
-  @apply text-gray-700 font-bold text-2xl;
+  @apply text-gray-700 dark:text-gray-300 font-bold text-2xl;
 }
 </style>
