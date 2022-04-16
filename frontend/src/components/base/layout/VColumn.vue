5a31358634d18a2231c6ee43b28849c0f91ac81d
Roman Kolesnikov <roman.kolesnikov@hs-augsburg.de>
2022-04-16 15:51:00 +0200

    [#13] Create VStack

---
+++ frontend/src/components/base/layout/VColumn.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { systemProps } from "@/lib/base/props/systemProps";
+import type { Responsive } from "@/lib/base/responsiveProp";
+import type { Spacing } from "@/lib/base/spacing";
+import type { PropType } from "vue";
+import VStack from "./VStack.vue";
+
+const props = defineProps({
+  gap: {
+    type: Object as PropType<Responsive<Spacing>>,
+  },
+  ...systemProps,
+});
+</script>
+
+<template>
+  <VStack direction="column" v-bind="props">
+    <slot />
+  </VStack>
+</template>
