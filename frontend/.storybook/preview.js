303c7541b8a2d00bcb51532ed93bf09701d2cddf
Roman Kolesnikov <roman.kolesnikov@hs-augsburg.de>
2022-04-16 08:24:47 +0200

    [#13] Use ress and add main.css back

--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -1,4 +1,5 @@
-import "normalize.css";
+import "ress/dist/ress.min.css";
+import "../src/main.css";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import "normalize.css";
+import "ress/dist/ress.min.css";
+import "./main.css";
 
 createApp(App).mount("#app");
