113c78d18446f847022afdc3101f190c5077ac30
Roman Kolesnikov <roman.kolesnikov@hs-augsburg.de>
2022-04-18 16:01:14 +0200

    [#6] Create SummaryTag

---
+++ frontend/src/components/summary/SummaryTag.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import VButton from "../base/button/VButton.vue";
+import VText from "../base/text/VText.vue";
+import type { Category } from "@/api/category";
+import VCenter from "@/components/base/layout/VCenter.vue";
+
+interface SummaryTagProps {
+  category: Category;
+}
+
+defineProps<SummaryTagProps>();
+</script>
+
+<template>
+  <VButton :padding="{ x: 2, y: 0 }">
+    <VCenter :height="6">
+      <VText as="span" size="sm" weight="medium" line-height="5">
+        {{ category.name }}
+      </VText>
+    </VCenter>
+  </VButton>
+</template>
