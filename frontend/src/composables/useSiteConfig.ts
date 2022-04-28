bf8e887da6c421a32dd1dfba8043a8e22269b5bf
Roman Kolesnikov <roman.kolesnikov@hs-augsburg.de>
2022-04-28 17:34:24 +0200

    [#32] Fix useSiteConfigStore

--- frontend/src/composables/useSiteConfig.ts
+++ frontend/src/composables/useSiteConfig.ts
@@ -1,9 +1,10 @@
-import { fetchSiteConfig, type SiteConfig } from "@/api/siteConfig";
+import { fetchSiteConfig } from "@/api/siteConfig";
 import { piniaKeysConfig } from "@/config/pinia";
 import { defineStore } from "pinia";
-import type { ComputedRef, Ref } from "vue";
+import type { ComputedRef } from "vue";
 import { useEndpoint } from "./useEndpoint";
 import logoTransparent from "@/static/logo_transparent.png";
+import { computed } from "@vue/reactivity";
 
 export interface UseSiteConfigState {
   blogTitle: ComputedRef<string>;
@@ -13,11 +14,19 @@ export interface UseSiteConfigState {
 // The site config is used in multiple places
 // and should persist between multiple pages.
 // So it is a good idea to create a store for it
-export const useSiteConfig = defineStore<string, Ref<SiteConfig>>(
+//
+// NOTE: With pinia it isn't possible to directly return SiteConfig
+export const useSiteConfig = defineStore<string, UseSiteConfigState>(
   piniaKeysConfig.siteConfig,
-  () =>
-    useEndpoint(fetchSiteConfig, {
+  () => {
+    const fetcher = useEndpoint(fetchSiteConfig, {
       blogTitle: "",
       logoUrl: logoTransparent,
-    })
+    });
+
+    return {
+      blogTitle: computed(() => fetcher.value.blogTitle),
+      logoUrl: computed(() => fetcher.value.logoUrl),
+    };
+  }
 );
