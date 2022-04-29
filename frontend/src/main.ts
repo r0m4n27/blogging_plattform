f6922a3d07300d09cb542bbcaf57fe1241ad169c
Roman Kolesnikov <roman.kolesnikov@hs-augsburg.de>
2022-04-29 09:16:10 +0200

    [#15] Switch to vueuse/head

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,11 +4,8 @@ import "ress/dist/ress.min.css";
 import "./main.css";
 import { router } from "./lib/router";
 import { createPinia } from "pinia";
-import { createMetaManager, defaultConfig, deepestResolver } from "vue-meta";
+import { createHead } from "@vueuse/head";
 
-const app = createApp(App)
-  .use(router)
-  .use(createPinia())
-  .use(createMetaManager(defaultConfig, deepestResolver.resolve));
+const app = createApp(App).use(router).use(createPinia()).use(createHead());
 
 router.isReady().then(() => app.mount("#app"));
