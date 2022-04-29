d2c44a609533c3d6de0f950da7fc868b92568786
Roman Kolesnikov <roman.kolesnikov@hs-augsburg.de>
2022-04-29 08:31:38 +0200

    [#15] Install vue-meta

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,5 +4,11 @@ import "ress/dist/ress.min.css";
 import "./main.css";
 import { router } from "./lib/router";
 import { createPinia } from "pinia";
+import { createMetaManager, defaultConfig, deepestResolver } from "vue-meta";
 
-createApp(App).use(router).use(createPinia()).mount("#app");
+const app = createApp(App)
+  .use(router)
+  .use(createPinia())
+  .use(createMetaManager(defaultConfig, deepestResolver.resolve));
+
+router.isReady().then(() => app.mount("#app"));
