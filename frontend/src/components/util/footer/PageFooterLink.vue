f342d9308ef6d411361a095b723d846dd01d9b48
Roman Kolesnikov <roman.kolesnikov@hs-augsburg.de>
2022-05-01 21:16:02 +0200

    [#39] Create FooterLink

---
+++ frontend/src/components/util/footer/PageFooterLink.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import VLink from "@/components/base/VLink.vue";
+import VExternalLink from "@/components/base/VExternalLink.vue";
+import PageFooterItem from "./PageFooterItem.vue";
+import type { FooterLink } from "./footerLink";
+
+interface PageFooterLinkProps {
+  link: FooterLink;
+}
+
+defineProps<PageFooterLinkProps>();
+</script>
+<template>
+  <VLink v-if="!link.isExternal" :to="link.destination">
+    <PageFooterItem :name="link.name" />
+  </VLink>
+  <VExternalLink v-else :href="(link.destination as string)">
+    <PageFooterItem :name="link.name" />
+  </VExternalLink>
+</template>
