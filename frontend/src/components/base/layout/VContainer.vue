4ae9f197544ea5b531f238ee1d450a25d3d2d546
Roman Kolesnikov <roman.kolesnikov@hs-augsburg.de>
2022-05-01 21:17:22 +0200

    [#39] Integrate PageFooter into layout

--- frontend/src/components/base/layout/VContainer.vue
+++ frontend/src/components/base/layout/VContainer.vue
@@ -1,19 +1,25 @@
 <script setup lang="ts">
-import { systemProps } from "@/styling/props/systemProps";
+import { createSystemPropsCss, systemProps } from "@/styling/props/systemProps";
 import VBox from "./VBox.vue";
 import {
   containerProps,
   createContainerPropsCss,
 } from "@/styling/props/containerProps";
+import { computed } from "vue";
+import { cx } from "@emotion/css";
 
 const props = defineProps({
   ...systemProps,
   ...containerProps,
 });
+
+const className = computed(() =>
+  cx(createContainerPropsCss(props), createSystemPropsCss(props))
+);
 </script>
 
 <template>
-  <VBox v-bind="props" :class="createContainerPropsCss(props)">
+  <VBox v-bind="props" :class="className">
     <slot />
   </VBox>
 </template>
