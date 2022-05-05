ac5bd90a99da5acf52fe0aaa058dc3e626b4b903
Roman Kolesnikov <roman.kolesnikov@hs-augsburg.de>
2022-05-05 17:17:37 +0200

    [#45] Add comment about warning

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,6 +6,10 @@ import { router } from "./lib/router";
 import { createPinia } from "pinia";
 import { createHead } from "@vueuse/head";
 
+// The following warning: inject() can only be used inside setup() or functional components.
+// Can be safely ignored, since it happens because of the router guards
+// In the pinia examples the behavior is considered safe
+// https://pinia.vuejs.org/core-concepts/outside-component-usage.html
 const app = createApp(App).use(router).use(createPinia()).use(createHead());
 
 router.isReady().then(() => app.mount("#app"));
