6b2bb12aef15b57de98ce4c60229e9c11184bac7
Roman Kolesnikov <roman.kolesnikov@hs-augsburg.de>
2022-05-07 11:31:40 +0200

    [#27] Allow usage of object.values

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,11 +2,20 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "ress/dist/ress.min.css";
 import "./main.css";
-import { router } from "./lib/router";
+import { createRouter } from "./lib/router/router";
 import { createPinia } from "./lib/pinia";
 import { createHead } from "@vueuse/head";
 
+// @ts-expect-error: This has no types
+import values from "object.values";
+
+if (!Object.values) {
+  values.shim();
+}
+
+const router = createRouter();
 const pinia = createPinia(router);
-const app = createApp(App).use(router).use(pinia).use(createHead());
+const head = createHead();
+const app = createApp(App).use(router).use(pinia).use(head);
 
 router.isReady().then(() => app.mount("#app"));
