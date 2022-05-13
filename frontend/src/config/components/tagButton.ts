fbe3615ce62094b913abda229bdb0963967a965b
Roman Kolesnikov <roman.kolesnikov@hs-augsburg.de>
2022-05-13 22:42:09 +0200

    [#51] Create TagButton

--- frontend/src/config/components/tagButton.ts
+++ frontend/src/config/components/tagButton.ts
@@ -1,11 +1,9 @@
-export type CategoryTagSize = "sm" | "md";
-
-export const categoryTagButtonConfig = {
+export const tagButtonVButtonConfig = {
   height: { sm: 6, md: 8 },
   padding: { sm: { x: 2, y: 0 }, md: { x: 2, y: 0 } },
 } as const;
 
-export const categoryTagTextConfig = {
+export const tagButtonTextConfig = {
   size: { sm: "sm", md: "md" },
   lineHeight: { sm: "5", md: "6" },
 } as const;
