6e0e89314d2f5528f77c8b7434fa9fbd79f195d3
Roman Kolesnikov <roman.kolesnikov@hs-augsburg.de>
2022-05-26 10:33:33 +0200

    [#69] Move SiteConfig

--- backend/src/init/ControllerModule.ts
+++ backend/src/init/ControllerModule.ts
@@ -1,11 +1,13 @@
 import { AuthController } from "@/controller/AuthController";
 import { RegisterCodeController } from "@/controller/RegisterCodeController";
+import { SiteConfigController } from "@/controller/SiteConfigController";
 import { EnvironmentModule } from "./EnvironmentModule";
 import { ServiceModule } from "./ServiceModule";
 
 export class ControllerModule {
   readonly authController: AuthController;
   readonly registerCodeController: RegisterCodeController;
+  readonly siteConfigController: SiteConfigController;
 
   constructor(services: ServiceModule, environment: EnvironmentModule) {
     this.authController = new AuthController(
@@ -15,6 +17,10 @@ export class ControllerModule {
     );
 
     this.registerCodeController = new RegisterCodeController(
+      services.databaseService,
+    );
+
+    this.siteConfigController = new SiteConfigController(
       services.databaseService,
     );
   }
--- backend/src/init/RouterModule.ts
+++ backend/src/init/RouterModule.ts
@@ -1,12 +1,14 @@
 import { SiteRouter } from "@/common/siteRouter";
 import { AuthRouter } from "@/router/AuthRouter";
 import { RegisterCodeRouter } from "@/router/RegisterCodeRouter";
+import { SiteConfigRouter } from "@/router/SiteConfigRouter";
 import { ControllerModule } from "./ControllerModule";
 import { MiddlewareModule } from "./MiddlewareModule";
 
 export class RouterModule {
   private readonly authRouter: AuthRouter;
   private readonly registerCodeRouter: RegisterCodeRouter;
+  private readonly siteConfigRouter: SiteConfigRouter;
 
   constructor(controllers: ControllerModule, middleware: MiddlewareModule) {
     this.authRouter = new AuthRouter(
@@ -18,9 +20,15 @@ export class RouterModule {
       controllers.registerCodeController,
       middleware.authMiddleware,
     );
+
+    this.siteConfigRouter = new SiteConfigRouter(
+      controllers.siteConfigController,
+      middleware.commonMiddleware,
+      middleware.authMiddleware,
+    );
   }
 
   get routers(): SiteRouter[] {
-    return [this.authRouter, this.registerCodeRouter];
+    return [this.authRouter, this.registerCodeRouter, this.siteConfigRouter];
   }
 }
