c6ac48812e95b2a3cb33d27fac271fd0808b1743
Roman Kolesnikov <roman.kolesnikov@hs-augsburg.de>
2022-05-26 15:43:37 +0200

    [#70] Create user routes

--- backend/src/init/RouterModule.ts
+++ backend/src/init/RouterModule.ts
@@ -2,6 +2,7 @@ import { SiteRouter } from "@/common/siteRouter";
 import { AuthRouter } from "@/router/AuthRouter";
 import { RegisterCodeRouter } from "@/router/RegisterCodeRouter";
 import { SiteConfigRouter } from "@/router/SiteConfigRouter";
+import { UserRouter } from "@/router/UserRouter";
 import { ControllerModule } from "./ControllerModule";
 import { MiddlewareModule } from "./MiddlewareModule";
 
@@ -9,6 +10,7 @@ export class RouterModule {
   private readonly authRouter: AuthRouter;
   private readonly registerCodeRouter: RegisterCodeRouter;
   private readonly siteConfigRouter: SiteConfigRouter;
+  private readonly userRouter: UserRouter;
 
   constructor(controllers: ControllerModule, middleware: MiddlewareModule) {
     this.authRouter = new AuthRouter(
@@ -26,9 +28,20 @@ export class RouterModule {
       middleware.commonMiddleware,
       middleware.authMiddleware,
     );
+
+    this.userRouter = new UserRouter(
+      controllers.userController,
+      middleware.authMiddleware,
+      middleware.commonMiddleware,
+    );
   }
 
   get routers(): SiteRouter[] {
-    return [this.authRouter, this.registerCodeRouter, this.siteConfigRouter];
+    return [
+      this.authRouter,
+      this.registerCodeRouter,
+      this.siteConfigRouter,
+      this.userRouter,
+    ];
   }
 }
