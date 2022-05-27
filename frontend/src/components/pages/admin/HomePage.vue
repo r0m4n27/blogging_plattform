f8eff1b059a14959aa71703b1b010c1d506b8079
Roman Kolesnikov <roman.kolesnikov@hs-augsburg.de>
2022-05-27 13:54:41 +0200

    [#75] Create register codes table

--- frontend/src/components/pages/admin/HomePage.vue
+++ frontend/src/components/pages/admin/HomePage.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 import UsersTable from "../../admin/users/UsersTable.vue";
 import { useHomePageState } from "@/composables/pages/admin/homePage";
+import VColumn from "../../base/layout/VColumn.vue";
+import RegisterCodesTable from "../../admin/registerCodes/RegisterCodesTable.vue";
 
 const { users, deleteUser } = useHomePageState();
 </script>
 
 <template>
-  <UsersTable :users="users" @delete-user="deleteUser" />
+  <VColumn width="full">
+    <RegisterCodesTable :codes="[]" />
+    <UsersTable :users="users" @delete-user="deleteUser" />
+  </VColumn>
 </template>
